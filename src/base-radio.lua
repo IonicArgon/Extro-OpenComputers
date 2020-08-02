f4f02b3518fc34eda7df53eda153e2d180f9bf89
Marco Tan <marco.tan.200405@gmail.com>
2020-08-02 13:17:06 -0400

    Fixes addresses.

--- src/base-radio.lua
+++ src/base-radio.lua
@@ -1,16 +1,16 @@
 local component = require("component")
 addresses = {
-    "74696253-673b-4fc6-9698-c4803f6e04dc",
-    "773bfa68-327d-4114-8aaa-4c7c8c0ef29c",
-    "56ee17f7-f47f-4444-a22b-810df11a451b",
-    "c70f66c4-5a8e-4361-8f4f-29cf0e574c7b",
-    "b39ab889-40ed-48d4-8dae-7ab41cb0ff45",
-    "704a2238-128c-4593-b2fa-8fe8332cdd36",
-    "21977000-2dd9-437e-923c-8888519c48a8",
-    "36ecee4a-edec-44c4-88f1-a0f7bbe541e5",
-    "5dd0821c-9b67-491e-b52f-2912a041c963",
-    "546c81ce-22a4-44ae-8654-c17790874ba2",
-    "3f77e919-bec0-402e-99e4-4a34be2646b8"
+    component.proxy("74696253-673b-4fc6-9698-c4803f6e04dc"),
+    component.proxy("773bfa68-327d-4114-8aaa-4c7c8c0ef29c"),
+    component.proxy("56ee17f7-f47f-4444-a22b-810df11a451b"),
+    component.proxy("c70f66c4-5a8e-4361-8f4f-29cf0e574c7b"),
+    component.proxy("b39ab889-40ed-48d4-8dae-7ab41cb0ff45"),
+    component.proxy("704a2238-128c-4593-b2fa-8fe8332cdd36"),
+    component.proxy("21977000-2dd9-437e-923c-8888519c48a8"),
+    component.proxy("36ecee4a-edec-44c4-88f1-a0f7bbe541e5"),
+    component.proxy("5dd0821c-9b67-491e-b52f-2912a041c963"),
+    component.proxy("546c81ce-22a4-44ae-8654-c17790874ba2"),
+    component.proxy("3f77e919-bec0-402e-99e4-4a34be2646b8"
 }
 names = {
     "Mariya Takeuchi - Plastic Love",
