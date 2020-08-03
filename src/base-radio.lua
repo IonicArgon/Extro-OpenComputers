d69b695a71734dcd79db4c8125b5dd7a69acafce
Marco Tan <marco.tan.200405@gmail.com>
2020-08-02 21:22:02 -0400

    NEVERMIND

--- src/base-radio.lua
+++ src/base-radio.lua
@@ -45,7 +45,7 @@ function shufflePlaylist()
     print("Shuffling playlist...")
     math.randomseed(os.time())
     for i = #addresses, 2, -1 do
-        local j = math.random(0, #addresses)
+        local j = math.random(1, i)
         
         addresses[i], addresses[j] = addresses[j], addresses[i]
         names[i], names[j] = names[j], names[i]
