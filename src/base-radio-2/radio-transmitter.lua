d8e7ee159b7fda2967ee4f6e969e21e7be359f8f
Marco Tan <marco.tan.200405@gmail.com>
2020-08-09 11:19:08 -0400

    Tweak colors and placement for testing.

--- src/base-radio-2/radio-transmitter.lua
+++ src/base-radio-2/radio-transmitter.lua
@@ -7,14 +7,14 @@ local w, h = gpu.getResolution()
 
 function Print_Header()
     gpu.setBackground(0x000000)
-    gpu.setForeground(0x660080)
+    gpu.setForeground(0x00CC00)
     for i = 1, 3, 2 do
         gpu.fill(1, i, w, 1, unicode.char(9655))
         gpu.fill(1, i + 1, w, 1, unicode.char(9665))
     end
     gpu.setForeground(0xFFFFFF)
-    gpu.set(4, 2, "Extro's Automatic Base Radio 2.0")
-    gpu.set(4, 3, "\"cool and good\" ~Pyro240, (Pyro#7539)")
+    gpu.set((w / 2) - 16, 2, "Extro's Automatic Base Radio 2.0")
+    gpu.set((w / 2) - 18, 3, "\"cool and good\" ~Pyro240, (Pyro#7539)")
     term.setCursor(1, 6)
 end
 
