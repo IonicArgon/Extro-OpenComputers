23d8836b0d4d79c156b6c54d89a65b17f68bc67c
Marco Tan <marco.tan.200405@gmail.com>
2020-08-08 20:51:28 -0400

    Add Print_Header() function for testing.

--- src/base-radio-2/radio-transmitter.lua
+++ src/base-radio-2/radio-transmitter.lua
@@ -1,5 +1,15 @@
 local component = require("component")
-local gpu = component.gpu
 local term = require("term")
+local unicode = require("unicode")
+local gpu = component.gpu
+local modem = component.modem
 local w, h = gpu.getResolution()
 
+function Print_Header()
+    gpu.setBackground(0x660080)
+    gpu.setForeground(0xFFFFFF)
+    gpu.fill(1, 1, w, math.floor(h/6), " ")
+    gpu.set(1, (math.floor(h / 6) / 2), "Extro's Automatic Base Radio 2.0")
+end
+
+Print_Header()
